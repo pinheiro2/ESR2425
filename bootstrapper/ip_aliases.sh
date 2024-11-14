c64cb6867f783ce93a46dfec2d07ada95a5e5864
Gustominox <agstcmps.111@gmail.com>
2024-11-14 11:40:29 +0000

    script to create ip aliases for testing in linux

---
+++ bootstrapper/ip_aliases.sh
@@ -0,0 +1,11 @@
+#! /usr/local/bin/
+# Add each IP alias to the loopback interface
+sudo ip addr add 10.0.0.10/8 dev lo
+sudo ip addr add 10.0.0.1/8 dev lo
+sudo ip addr add 10.0.1.1/8 dev lo
+sudo ip addr add 10.0.3.2/8 dev lo
+sudo ip addr add 10.0.5.2/8 dev lo
+sudo ip addr add 10.0.7.2/8 dev lo
+sudo ip addr add 10.0.11.2/8 dev lo
+sudo ip addr add 10.0.10.2/8 dev lo
+sudo ip addr add 10.0.15.2/8 dev lo
